26d0ec8210c8e81050fb160eb4e2cd3196e05167
F1ni <ArnavGupta1706@gmail.com>
2026-02-18 17:18:13 +0000

    implemented

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import Ridge, LinearRegression
 from sklearn.ensemble import RandomForestRegressor, HistGradientBoostingRegressor, GradientBoostingRegressor
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.metrics import r2_score
 
 
@@ -41,9 +41,9 @@ preprocessor = ColumnTransformer([
 ])
 
 # pipeline used to create csv file
-final_model = Pipeline([
+best_model = Pipeline([
     ("preprocessor", preprocessor),
-    ("regressor", GradientBoostingRegressor(random_state=42, max_depth=3))
+    ("regressor", GradientBoostingRegressor(learning_rate=0.05, max_depth=3, n_estimators=200, random_state=42))
 ])
 
 # all the models that were tested
@@ -52,24 +52,52 @@ models = {
     "ridge": Ridge(alpha=1.0),
     "random forest": RandomForestRegressor(n_estimators=100, random_state=42),
     "Standard GB (Default)": GradientBoostingRegressor(random_state=42, max_depth=3),
-    "GB (Tuned)": GradientBoostingRegressor(learning_rate=0.05, max_depth=4, random_state=42),
+    "Standard GB (Tuned)": GradientBoostingRegressor(learning_rate=0.05, max_depth=3, n_estimators=200, random_state=42),
 }
 
 
 # print csv file using best model
-def create_csv_using_final_model():
+def create_csv_using_best_model():
     X_tst = pd.read_csv('CW1_test.csv')
 
     X_tst = X_tst.drop(columns_to_remove, errors="ignore") 
 
-    final_model.fit(X, y)
+    best_model.fit(X, y)
 
-    yhat_lm = final_model.predict(X_tst)
+    yhat_lm = best_model.predict(X_tst)
 
     # Format submission:
     # This is a single-column CSV with nothing but your predictions
     out = pd.DataFrame({'yhat': yhat_lm})
     out.to_csv('CW1_submission_K23166817.csv', index=False) # Please use your k-number here
+
+
+def hyperparameter_tuning_using_gridsearch():
+    param_grid = {
+        'regressor__learning_rate': [0.05, 0.1, 0.2], 
+        'regressor__n_estimators': [100, 200],
+        'regressor__max_depth': [3, 4, 5]
+    }
+
+    gb = GradientBoostingRegressor(random_state=42)
+
+    pipeline_grid = Pipeline([
+        ("preprocessor", preprocessor),
+        ("regressor", gb)
+    ])
+
+    search = GridSearchCV(
+        pipeline_grid, 
+        param_grid, 
+        cv=5, 
+        scoring='r2',
+        n_jobs=-1
+    )
+
+    search.fit(X, y)
+
+    print("Best Parameters:", search.best_params_)
+    print("Best R2 Score:", search.best_score_)
 
 
 # R2 score for all models that I tested
@@ -95,6 +123,7 @@ def testing_all_models():
     print(results)
 
 # main
-testing_all_models()
-create_csv_using_final_model()
+# testing_all_models()
+create_csv_using_best_model()
+# hyperparameter_tuning_using_gridsearch()
 
